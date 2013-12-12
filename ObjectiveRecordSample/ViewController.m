7e6e0d0848783b9ab3b519fe8e540644154f09a4
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2013-12-12 18:50:01 +0900

    Personモデルの内容を出力するテストコードを追加

--- ObjectiveRecordSample/ViewController.m
+++ ObjectiveRecordSample/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Person.h"
 
 @interface ViewController ()
 
@@ -18,6 +19,10 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    for(Person *person in [Person all]) {
+        NSLog(@"Person name = %@, age = %@", person.name, person.age);
+    }
 }
 
 - (void)didReceiveMemoryWarning
