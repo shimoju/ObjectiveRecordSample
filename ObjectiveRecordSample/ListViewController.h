4622f4cb6be07b2ecaf916d0d223990d25c84806
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2013-12-13 02:52:17 +0900

    ListViewControllerを作成してTable View Controllerのカスタムクラスに設定

---
+++ ObjectiveRecordSample/ListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListViewController.h
+//  ObjectiveRecordSample
+//
+//  Created by Hiroshi Shimoju on 2013/12/13.
+//  Copyright (c) 2013年 shimoju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListViewController : UITableViewController
+
+@end
