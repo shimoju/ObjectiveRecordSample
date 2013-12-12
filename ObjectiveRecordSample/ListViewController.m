07a4fd133bdf2106267eb4a7d5775799aa494a29
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2013-12-13 04:18:05 +0900

    リスト表示機能を実装

--- ObjectiveRecordSample/ListViewController.m
+++ ObjectiveRecordSample/ListViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ListViewController.h"
+#import "Person.h"
 
 @interface ListViewController ()
 
@@ -48,16 +49,14 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return [[Person all] count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -66,7 +65,10 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
     // Configure the cell...
-    
+    NSArray *people = [Person all];
+    cell.textLabel.text = [people[indexPath.row] valueForKey:@"name"];
+    cell.detailTextLabel.text = [[people[indexPath.row] valueForKey:@"age"] stringValue];
+
     return cell;
 }
 
