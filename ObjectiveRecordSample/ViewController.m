8921910ef1588652f03bdfab242c5728eb855468
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2014-01-23 11:27:02 +0900

    nameフィールドをFirstResponderにした

--- ObjectiveRecordSample/ViewController.m
+++ ObjectiveRecordSample/ViewController.m
@@ -24,6 +24,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [_nameField becomeFirstResponder];
 }
 
 - (void)didReceiveMemoryWarning
